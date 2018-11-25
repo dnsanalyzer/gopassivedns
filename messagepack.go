dd9b4559d6bddadbbe08ffd0fa4f80ccf538205d
dnsanalyzer <42930327+dnsanalyzer@users.noreply.github.com>
2018-11-25 17:00:16 +0700

    Update messagepack.go

--- messagepack.go
+++ messagepack.go
@@ -7,48 +7,32 @@ import (
 // logEntry is the same as dnsLog without some fields which are not required
 // for fluentd outputs.
 type logEntry struct {
-	Query_ID             uint16 `msgpack:"query_id"`
-	Response_Code        int    `msgpack:"rcode"`
+	Response_Code        int    `msgpack:"r"`
 	Question             string `msgpack:"q"`
-	Question_Type        string `msgpack:"qtype"`
+	Question_Type        string `msgpack:"qt"`
 	Answer               string `msgpack:"a"`
-	Answer_Type          string `msgpack:"atype"`
-	TTL                  uint32 `msgpack:"ttl"`
-	Server               string `msgpack:"dst"`
-	Client               string `msgpack:"src"`
-	Timestamp            string `msgpack:"tstamp"`
-	Elapsed              int64  `msgpack:"elapsed"`
-	Client_Port          string `msgpack:"sport"`
+	TTL                  uint32 `msgpack:"t"`
+	Server               string `msgpack:"d"`
+	Client               string `msgpack:"s"`
+	Timestamp            string `msgpack:"ts"`
+	Elapsed              int64  `msgpack:"e"`
 	Level                string `msgpack:"level,omitempty"` // syslog level omitted if empty
-	Length               int    `msgpack:"bytes"`
-	Proto                string `msgpack:"protocol"`
-	Truncated            bool   `msgpack:"truncated"`
-	Authoritative_Answer bool   `msgpack:"aa"`
-	Recursion_Desired    bool   `msgpack:"rd"`
-	Recursion_Available  bool   `msgpack:"ra"`
+	Length               int    `msgpack:"b"`
 }
 
 func (dle *dnsLogEntry) MarshalMsgpack() ([]byte, error) {
 	return msgpack.Marshal(&logEntry{
-		Query_ID:             dle.Query_ID,
 		Response_Code:        dle.Response_Code,
 		Question:             dle.Question,
 		Question_Type:        dle.Question_Type,
 		Answer:               dle.Answer,
-		Answer_Type:          dle.Answer_Type,
 		TTL:                  dle.TTL,
 		Server:               dle.Server.String(),
 		Client:               dle.Client.String(),
 		Timestamp:            dle.Timestamp,
 		Elapsed:              dle.Elapsed,
-		Client_Port:          dle.Client_Port,
 		Level:                dle.Level,
 		Length:               dle.Length,
-		Proto:                dle.Proto,
-		Truncated:            dle.Truncated,
-		Authoritative_Answer: dle.Authoritative_Answer,
-		Recursion_Desired:    dle.Recursion_Desired,
-		Recursion_Available:  dle.Recursion_Available,
 	})
 }
 
